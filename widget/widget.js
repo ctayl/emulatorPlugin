4976a86d132ec9e6f91df1a35305d42d36a2caf1
Christopher Berger <chrisb000@gmail.com>
2022-11-03 19:04:25 -0700

    rough draft with skeletons

--- widget/widget.js
+++ widget/widget.js
@@ -1,10 +1,17 @@
 const widget = {
 
+	// config: {
+	// 	endpoint: 'https://app.buildfire.com/api/auth/user/apps',
+	// 	env: 'prod'
+	// },
 	config: {
-		endpoint: 'https://app.buildfire.com/api/auth/user/apps',
-		env: 'prod'
+		endpoint: 'https://uat2-app.buildfire.com/api/auth/user/apps',
+		env: 'uat'
 	},
 
+	searchInput: document.getElementById('searchInput'),
+	searchHeader: document.getElementById('searchHeader'),
+	body: document.getElementById('body'),
 	textContainer: document.getElementById('textContainer'),
 	appContainer: document.getElementById('list'),
 	scrollContainer: document.getElementById('scrollContainer'),
@@ -16,6 +23,7 @@ const widget = {
 	pageIndex: 0,
 	pageSize: 10,
 	done: false,
+	timeout: null,
 
 	text: `<div class="header">
 					<h1 class="header__title">Your Designs</h1>
@@ -24,8 +32,26 @@ const widget = {
 
 	init() {
 
+		this.appContainer.innerHTML = this.getSkeletons().appList();
+
 		buildfire.datastore.get(this.loadData);
 		buildfire.datastore.onUpdate(obj => this.loadData(null, obj), false);
+
+		this.searchInput.onchange = ({ target }) => {
+			if (target.value.length > 0 && target.value.length < 4) return;
+			console.error(target.value);
+			clearTimeout(this.timeout);
+			this.appContainer.innerHTML = '';
+			this.apps = [];
+			this.pageIndex = 0;
+			this.done = false;	
+		  
+			this.timeout = setTimeout(() => {
+				this.timeout = null;
+				this.busy = false;
+				this.searchApps(target.value);
+			}, 300);
+		};
 
 		this.searchApps();
 
@@ -45,11 +71,11 @@ const widget = {
 		}
 
 		widget.textContainer.innerHTML = widget.text;
+		widget.appContainer.innerHTML = widget.getSkeletons().appList();
 	},
 
-	searchApps() {
+	searchApps(query) {
 		if (this.busy) return;
-		buildfire.spinner.show();
 		this.busy = true;
 
 		const options = {
@@ -58,22 +84,44 @@ const widget = {
 			sort: { name: 1 },
 		};
 
-		const handleResponse = (error, { data }) => {
-			buildfire.spinner.hide();
+		if (query) options.appName = query;
+		else if (this.apps.length) {
+			this.appContainer.innerHTML += this.getSkeletons().appList(1);
+		}
+
+		const handleResponse = (error, response = {}) => {
 			if (error) return console.error(error);
+			
+			const { data } = response;
 
 			if (!data.length || data.length < this.pageSize) {
 				this.done = true;
 			}
-			if (data.length === 1 && this.pageIndex === 0) {
+			if (!this.apps.length && !query && data.length === this.pageSize && this.pageIndex === 0) {
+				this.searchHeader.classList.add('active');
+				// strip off animations once they end
+				document.body.addEventListener('transitionend', () => {
+					document.body.classList.remove('animating');
+				}, { once: true });
+				
+				// slide in search bar
+				document.body.classList.add('has-search', 'animating');
+			}
+			if (!query && data.length === 1 && this.pageIndex === 0) {
+				// only auto navigate on initial his
 				this.autoNavigate(data[0]);
 				this.done = true;
 			}
+			if (this.apps.length === 0) this.appContainer.innerHTML = '';
+			else widget.appContainer.removeChild(widget.appContainer.querySelector('.skeleton'));
 			this.renderApps(data);
 			this.pageIndex += 1;
 			this.busy = false;
 		}
 		this.search(options, handleResponse);
+		// setTimeout(() => {
+		// 	this.search(options, handleResponse);
+		// }, 2000);
 	},
 
 	autoNavigate(app) {
@@ -117,9 +165,9 @@ const widget = {
 		xhr.onerror = console.error;
 		xhr.setRequestHeader("Content-Type", "application/json");
 
-		buildfire.auth.getCurrentUser((err, { accessToken }) => {
+		buildfire.auth.getCurrentUser((err, { accessToken, userId }) => {
 			if (err) return console.error(err);
-			xhr.send(JSON.stringify({ ...options, accessToken }));
+			xhr.send(JSON.stringify({ ...options, accessToken, userId }));
 		})
 	},
 
@@ -143,6 +191,7 @@ const widget = {
 
 	renderCard(app) {
 		const { appId, name, appearance } = app;
+		const fallbackIcon = '../../../styles/media/holder-1x1.png';
 
 		const card = document.createElement('div');
 		card.classList.add('card');
@@ -154,7 +203,7 @@ const widget = {
 		const img = document.createElement('img');
 		img.classList.add('card__img');
 
-		const iconUrl = appearance.iconUrl ? buildfire.imageLib.cropImage(appearance.iconUrl, { size: 'xs', aspect: '1:1' }) : '../../../styles/media/holder-1x1.png';
+		const iconUrl = appearance.iconUrl ? buildfire.imageLib.cropImage(appearance.iconUrl, { size: 'xs', aspect: '1:1' }) : fallbackIcon;
 		img.setAttribute('src', iconUrl);
 
 		const title = document.createElement('h4');
@@ -181,6 +230,41 @@ const widget = {
 		}
 		if (!app.env) app.env = this.config.env;
 		buildfire.navigation.navigateEmulator(app);
+	},
+
+	getSkeletons() {
+		return {
+			appList: count => {
+				let html = (
+					`<div class="card skeleton">
+						<div class="card__img--holder">
+							<div class="card__img bf-skeleton-loader"></div>
+						</div>
+						<h4 class="card__title bf-skeleton-loader skeleton-text"></h4>
+					</div>`
+				);
+				let result = '';
+
+				let cardLength = count;
+
+				if (!cardLength) {
+					const searchHeight = this.searchHeader.clientHeight;
+					const textHeight = this.textContainer.clientHeight;
+
+					const appListHeight = window.innerHeight - (searchHeight + textHeight);
+
+					cardLength = Math.floor(appListHeight / 94);
+				}
+
+				while (cardLength > 0) {
+
+					result += html;
+					cardLength --;
+				}
+
+				return result;
+			}
+		}
 	}
 };
 
